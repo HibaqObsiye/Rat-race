31dc8b24651a9813d46d0fbd404148bee7f0a775
hibaq obsiye <obsiyehibaq@gmail.com>
2023-02-15 11:48:56 +0000

    wrote passing test for board.isReversed function

--- src/components/board/Board.test.js
+++ src/components/board/Board.test.js
@@ -1,3 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import Board from './Board';
 
+
+// Mock isReversed function
+Board.isReversed = jest.fn((index) => {
+    return index % 2 === 0;
+  });
+  
+  test('isReversed returns true when index is even', () => {
+    expect(Board.isReversed(0)).toBe(true);
+    expect(Board.isReversed(2)).toBe(true);
+    expect(Board.isReversed(4)).toBe(true);
+  });
+  
+  test('isReversed returns false when index is odd', () => {
+    expect(Board.isReversed(1)).toBe(false);
+    expect(Board.isReversed(3)).toBe(false);
+    expect(Board.isReversed(5)).toBe(false);
+  });
